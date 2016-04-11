6e0932db424c32ebadc9541e76601652dfa25941
hxy0107 <hxy0107@yeah.net>
2016-04-11 11:22:24 +0800

    Initial commit

---
+++ app/src/main/java/com/event/demo/PostEventChirld.java
@@ -0,0 +1,11 @@
+package com.event.demo;
+
+/**
+ * 作者：毕空 on 2016/4/11 11:16
+ * 邮箱：xianyu.hxy@alibaba-inc.com
+ */
+public class PostEventChirld extends PostEvent {
+    public PostEventChirld(String msg) {
+        super(msg);
+    }
+}
